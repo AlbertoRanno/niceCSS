7a78d2cce95f8e0b23f556227a68c96ef4edc322
unknown <rodolfo.dominguez@cdt.com.ar>
2023-07-25 13:05:19 -0300

    Ajustes de editor y de css

--- editor.js
+++ editor.js
@@ -1,9 +1,15 @@
+CKEDITOR.config.removePlugins='scayt,wsc';
 CKEDITOR.editorConfig = function(config) {
     config.google_fonts = 'Nunito', 'Nunito:wght@500';
 
     config.stylesSet.push({ name : 'warningImportant', element : 'p', wrap:true, attributes : { 'class' : 'warningImportant' } });
     config.stylesSet.push({ name : 'warningChannels', element : 'p', wrap:true, attributes : { 'class' : 'warningChannels' } });
     config.stylesSet.push({ name : 'warningRegistration', element : 'p', wrap:true, attributes : { 'class' : 'warningRegistration'} });
+    config.stylesSet.push({ name : 'noContent', wrap:false, attributes : { 'class' : 'noContent'} });
+    
+    config.stylesSet.push({ name : 'combinarCeldas', element : 'div', wrap:true, attributes : { 'class' : 'combinarCeldas' },{'colspan' : '2'} {'rowspan' : '1' }});
 
-    config.stylesSet.push({ name : 'CanalesUrgencias', element : 'H5', wrap:false, attributes : { 'class' : 'CanalesUrgencias', 'title' :'Custom_Class2' } });
+    config.stylesSet.push({ name : '2 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateTwoColumns' } });
+    config.stylesSet.push({ name : '3 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateThreeColumns' } });
+    config.stylesSet.push({ name : '4 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateFourColumns' } });
 };
