572b2d6158fd4dc17b57b4602481d63a52087459
unknown <rodolfo.dominguez@cdt.com.ar>
2023-07-26 16:18:17 -0300

    Ajustes de editor y de css

--- editor.js
+++ editor.js
@@ -6,9 +6,10 @@ CKEDITOR.editorConfig = function(config) {
     config.stylesSet.push({ name : 'warningChannels', element : 'p', wrap:true, attributes : { 'class' : 'warningChannels' } });
     config.stylesSet.push({ name : 'warningRegistration', element : 'p', wrap:true, attributes : { 'class' : 'warningRegistration'} });
     config.stylesSet.push({ name : 'noContent', wrap:false, attributes : { 'class' : 'noContent'} });
-    config.stylesSet.push({ name : 'combinarCeldas', element : 'div', wrap:true, attributes : { 'class' : 'combinarCeldas' }});
+    config.stylesSet.push({ name : 'combinarCeldas', element : 'div', wrap:true, attributes : { 'class' : 'combinarCeldas' ,'colspan' : '2', 'rowspan' : '1' }});
 
     config.stylesSet.push({ name : '2 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateColumns twoColumns' } });
     config.stylesSet.push({ name : '3 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateColumns threeColumns' } });
     config.stylesSet.push({ name : '4 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateColumns fourColumns' } });
+
 };
